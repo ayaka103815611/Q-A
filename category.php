5bf99b8058311e7b6f72742e314cb70113ff54f0
ayaka103815611 <maru8150103@gmail.com>
2025-02-01 00:56:52 +0900

    Add files via upload

--- category.php
+++ category.php
@@ -6,8 +6,13 @@
       <div class="breadcrumbs_container">
         <div class="image_header">
           <div class="header_info">
-            <div>news</div>
-            <div>ニュース</div>
+            <?php
+            $cat = get_the_category();
+            $catslug = $cat[0] ->slug;
+            $catname = $cat[0] ->cat_name;
+            ?>
+            <div><?php echo $catslug; ?></div>
+            <div><?php echo $catname; ?></div>
           </div>
         </div>
       </div>
@@ -24,11 +29,11 @@
             <div class="tab_panels">
               <!-- Description -->
               <div class="tab_panel">
-                <div class="tab_panel_title">ニュース</div>
+                <div class="tab_panel_title"><?php echo $catname; ?></div>
                 <div class="tab_panel_content">
                   <div class="tab_panel_text">
                         <!-- news loop from here-->
-                  <?php if (have_posts()) : ?>
+                        <?php if (have_posts()) : ?>
                       <?php while(have_posts()) : the_post(); ?>
                       <div class="news_posts_small">
                       <div class="row">
@@ -36,11 +41,23 @@
                           <div class="calendar_news_border">
                             <div class="calendar_news_border_1">
                               <div class="calendar_month">
-                                <?php echo get_post_time('F'); ?>
+                                <?php 
+                                if( is_category('event') ) :
+                                  echo post_custom('month');
+                                else:
+                                  echo get_post_time('F');
+                                endif;
+                              ?>
                               </div>
                               <div class="calendar_day">
                                 <span>
-                                  <?php echo get_the_date('d'); ?>
+                                  <?php
+                                  if( is_category('event') ) :
+                                    echo post_custom('day');
+                                  else:
+                                    echo get_the_date('d');
+                                  endif;
+                                  ?>
                                 </span>
                               </div>
                             </div>
